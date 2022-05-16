88663baf79d63c74e89deb7d60b714b154ceade4
Thomas Burghout <thomas.burghout@nedap.com>
2022-05-16 13:20:27 +0200

    init

---
+++ src/test/java/org/example/SomethingTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SomethingTest {
+    @Test
+    public void testSomething() {
+        assertEquals(42, 7);
+    }
+}
